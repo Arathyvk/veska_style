864e575f68ea43bc7edfd8a951a543cb6565dc06
Arathy <arathymalu99@gmail.com>
2026-03-31 11:01:26 +0530

    feat(category_edit) : implement edit category functionality

--- category_admin/urls.py
+++ category_admin/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('categories/',views.category_list,name='admin_category_list'),
     path('categories/add/',views.category_add,name='admin_category_add'),
-
+    path('categories/<int:pk>/edit/',views.category_edit,name='admin_category_edit'),
+    path('categories/<int:pk>/block/',views.category_block,name='category_block'),
+    path('categories/<int:pk>/unblock/',views.category_unblock,name='category_unblock'),
 
 ]
